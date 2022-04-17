ad19eec68893faaaaf39f3ff3105d5db27736579
HennaReis <67883256+HeannaReis@users.noreply.github.com>
2022-04-16 22:17:16 -0300

    Primeiro Commit

---
+++ categorias.php
@@ -0,0 +1,85 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'Infantil';
+$categorias[] = 'Juvenil';
+$categorias[] = 'Adulto';
+$categorias[] = 'Senior';
+
+
+//print_r($categorias);
+
+
+
+//$categorias = [];
+//$categorias = array( "Infantil", "Juvenil", "Adulto", "Senior");
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+
+$idade = $_POST['idade'];
+
+if (empty($nome) || ($idade) == "") 
+{
+    echo "Preencha todos os campos corretamente ";
+    return;
+}
+if (strlen($nome) < 3)
+{
+    echo "O Nome deve conter mais do que 3 caracteres ";
+    return;
+}
+if (strlen($nome) > 40)
+{
+    echo "O Nome não deve conter mais do que 40 caracteres ";
+    return;
+}
+if (!is_numeric($idade))
+{
+    echo "Campo Idade só aceita números";
+}
+
+else if($idade < 6)
+{
+    {
+        echo " Desculpe " .$nome. " , você ainda não pode competir!! ";
+        return;
+    }    
+}
+else if($idade >= 6 && $idade <= 12)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'Infantil')
+        {
+            echo "O nadador " .$nome. " Compete na Categoria: " .$categorias[$i];   
+            break;                
+        }    
+    }    
+}
+else if ($idade >= 13 && $idade <= 18)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'Juvenil')
+        {
+            echo "O nadador " .$nome. " Compete na Categoria: " .$categorias[$i]; 
+            break;
+        }  
+    }              
+}
+else if ($idade >= 19 && $idade <= 60)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'Adulto')
+        {
+            echo "O nadador " .$nome. " Compete na Categoria: " .$categorias[$i];
+            break;
+        }
+    }    
+}
+else
+{
+    echo "O nadador " .$nome. " Compete na Categoria:  Senior";
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta Charset="UTF-8">
+<title>Formulário de Inscrição</title>
+<meta nome="author" content="">
+<meta nome="description" content="">
+<meta name="viewimport" content="whidt=device-whidt, initial-scale=1">
+</head>
+
+<body>
+<h2><p>Formulário de Inscrição</p></h2>
+
+<form action="categorias.php" method="POST">
+
+<p>Your Name: <input type="text" name="nome" /> </p>
+<p>Your Age: <Input type="text" name="idade" /></p>
+<p><input type="submit" /></p>
+</form>
+
+</body>
+
+</html>
+
