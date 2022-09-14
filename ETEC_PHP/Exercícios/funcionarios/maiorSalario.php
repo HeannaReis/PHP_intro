a24a1d5873aef8d2337373610daf62a49f3eb625
HeannaReis <heannareis@gmail.com>
2022-09-14 20:05:13 -0300

    Correção maiorSalario

--- ETEC_PHP/Exercícios/funcionarios/maiorSalario.php
+++ ETEC_PHP/Exercícios/funcionarios/maiorSalario.php
@@ -9,11 +9,8 @@ foreach ($funcionarios as $k => $value)
 {   
     if($value['salario'] > $maior)
     $maior = $value['salario'];
+}   
 
-}
-echo ("<br>" . " O Maior salário é: " . $maior . " !!! ");
-
-    echo ("<br>" . $value['salario']);
 echo "<br>";
 echo ("<br>" . " O Maior salário é: " . $maior . " !!! ");
 
